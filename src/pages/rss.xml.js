ebbc503cfa7d1aadafbd8bb0a1aa7f033297f7fc
Adon Metcalfe <adon.metcalfe@dpc.wa.gov.au>
2024-09-13 15:55:45 +0000

    full html in rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,9 @@
 import { getCollection } from "astro:content";
 import rss from '@astrojs/rss';
+import MarkdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const docs = await getCollection('docs');
@@ -7,11 +11,16 @@ export async function GET(context) {
     title: "DGov Site Reliability Engineering",
     description: "DGov Site Reliability Engineering",
     site: context.site,
-    items: docs.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      link: `/${post.slug}/`,
+    items: await Promise.all(docs.map(async (post) => {
+      const { Content } = await post.render();
+      const content = sanitizeHtml(parser.render(post.body));
+      return {
+        title: post.data.title,
+        pubDate: post.data.pubDate || new Date(),
+        description: post.data.description,
+        link: `/${post.slug}/`,
+        content: content,
+      };
     })),
   });
 }
